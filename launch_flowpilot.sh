ed6a21107895f60b789cd399d5000f46a6e047d3
Padraic Hoselton <paddysappleacount@icloud.com>
2023-04-19 21:52:28 -0700

    Added Fingerprint and git pull to start script

--- launch_flowpilot.sh
+++ launch_flowpilot.sh
@@ -1,3 +1,5 @@
+git pull
+
 set -e
 source ./.env
 
@@ -14,10 +16,10 @@ export PASSIVE="0"
 export ZMQ_MESSAGING_PROTOCOL="TCP" # TCP, INTER_PROCESS, SHARED_MEMORY
 #export IP_CAN_ADDR="127.0.0.1"
 
-export SIMULATION="1"
-#export FINGERPRINT="HONDA CIVIC 2016"
+#export SIMULATION="1"
+export FINGERPRINT="HYUNDAI IONIQ HYBRID 2017-2019"
 
 ## android specific ##
-export USE_SNPE="0" # only works for snapdragon devices.
+export USE_SNPE="1" # only works for snapdragon devices.
 
 flowinit
