baaca9a1065da675ebd161828893d88af396632a
Padraic Hoselton <paddysappleacount@icloud.com>
2023-04-20 21:36:30 -0700

    Fully removed init

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -87,8 +87,8 @@ class CarInterface(CarInterfaceBase):
 
     return ret
 
-  @staticmethod
-  def init(CP, logcan, sendcan):
+  ##@staticmethod
+  ##def init(CP, logcan, sendcan):
     ##if CP.openpilotLongitudinalControl:
     ##  disable_ecu(logcan, sendcan, addr=0x7d0, com_cont_req=b'\x28\x83\x01')
 
