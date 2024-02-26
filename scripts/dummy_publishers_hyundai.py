608c4940df1f43b5561cc3768fb645b0a86742cd
Padraic Hoselton <35149493+Paperboypaddy@users.noreply.github.com>
2024-02-25 22:46:50 -0800

    Got correctly running hyundai dummy publisher.

--- scripts/dummy_publishers_hyundai.py
+++ scripts/dummy_publishers_hyundai.py
@@ -5,7 +5,7 @@ from opendbc.can.packer import CANPacker
 from selfdrive.boardd.boardd import can_list_to_can_capnp
 from selfdrive.car import crc8_pedal
 from common.realtime import DT_DMON
-from selfdrive.car.honda.values import CruiseButtons
+from selfdrive.car.hyundai.values import Buttons
 import time
 
 packer = CANPacker("hyundai_kia_generic")
@@ -17,30 +17,28 @@ def can_function(pm, speed, angle, idx, cruise_button, is_engaged):
   # *** powertrain bus ***
 
   speed = speed * 3.6 # convert m/s to kph
-  msg.append(packer.make_can_msg("WHL_SPD11", 0, {
+  msg.append(packer.make_can_msg("WHL_SPD11", 2, {
     "WHL_SPD_FL": speed,
     "WHL_SPD_FR": speed,
     "WHL_SPD_RL": speed,
     "WHL_SPD_RR": speed
   }))
 
-  msg.append(packer.make_can_msg("CLU11", 0, {"CF_Clu_CruiseSwState": cruise_button}))
+  msg.append(packer.make_can_msg("CLU11", 2, {"CF_Clu_CruiseSwState": cruise_button}))
 
-  msg.append(packer.make_can_msg("E_EMS11", 0, {"CR_Vcu_AccPedDep_Pos": 100}))
+  msg.append(packer.make_can_msg("E_EMS11", 2, {"CR_Vcu_AccPedDep_Pos": 0}))
 
-  msg.append(packer.make_can_msg("CLU15", 0, {"CF_Clu_Gear": 4}))
-  msg.append(packer.make_can_msg("CGW1", 0, {"CF_Gway_DrvSeatBeltSw": 1}))
-  msg.append(packer.make_can_msg("STEER_STATUS", 0, {}))
-  msg.append(packer.make_can_msg("SAS11", 0, {"SAS_Angle": angle}))
-  msg.append(packer.make_can_msg("TCS13", 0, {"StandStill": 1 if speed >= 1.0 else 0, "ACCEnable": int(is_engaged)}))
-  msg.append(packer.make_can_msg("MDPS12", 0, {}))
-  msg.append(packer.make_can_msg("TCS15", 0, {}))
-  msg.append(packer.make_can_msg("ESP12", 0, {}))
-  msg.append(packer.make_can_msg("CGW2", 0, {}))
-  msg.append(packer.make_can_msg("CGW4", 0, {}))
+  msg.append(packer.make_can_msg("CLU15", 2, {"CF_Clu_Gear": 8}))
+  msg.append(packer.make_can_msg("CGW1", 2, {"CF_Gway_DrvSeatBeltSw": 1}))
+  msg.append(packer.make_can_msg("SAS11", 2, {"SAS_Angle": angle}))
+  msg.append(packer.make_can_msg("TCS13", 2, {"StandStill": 1 if speed >= 1.0 else 0, "ACCEnable": 0,"ACC_REQ": 1, "DriverBraking": 0, "PBRAKE_ACT": 0}))
+  msg.append(packer.make_can_msg("TCS15", 2, {"AVH_LAMP": 0}))
+  msg.append(packer.make_can_msg("MDPS12", 2, {"CF_Mdps_ToiUnavail": 0, "CF_Mdps_ToiFlt": 0}))
+  msg.append(packer.make_can_msg("ESP12", 2, {})) # Blank for now
+  msg.append(packer.make_can_msg("CGW2", 2, {})) # Blank
+  msg.append(packer.make_can_msg("CGW4", 2, {})) # Blank
 
   # *** cam bus ***
-  msg.append(packer.make_can_msg("LKAS11", 2, {}))
 
   pm.send('can', can_list_to_can_capnp(msg))
 
@@ -49,7 +47,7 @@ pm = messaging.PubMaster(["can"])
 def can_function_runner(exit_event: threading.Event):
   i = 0
   while not exit_event.is_set():
-    cruise_button = CruiseButtons.RES_ACCEL
+    cruise_button = Buttons.RES_ACCEL
     if i % 500 == 0:
         cruise_button = 0
     can_function(pm, 20, 0.1, i, cruise_button, True)
@@ -65,7 +63,7 @@ def panda_state_function(exit_event: threading.Event):
       'ignitionLine': True,
       'pandaType': "blackPanda",
       'controlsAllowed': True,
-      'safetyModel': 'hondaNidec',
+      'safetyModel': 'allOutput',
     }
     pm.send('pandaStates', dat)
     time.sleep(0.5)
